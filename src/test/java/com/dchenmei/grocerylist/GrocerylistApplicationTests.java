ead8cdfc37f1ecb7efc364530e2a4402a922a1fb
dchenmei <26525975+dchenmei@users.noreply.github.com>
2020-03-09 13:29:13 -0400

    Spring initializr generated project

---
+++ src/test/java/com/dchenmei/grocerylist/GrocerylistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dchenmei.grocerylist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrocerylistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
